f19f5466f04210f5996d7d3f8f8ada0496c0a4ea
trieutran91 <trieu.tran@payex.com>
2020-04-22 15:55:14 +0200

    Changed output directory. Plugin works but doesn't load correctly

--- src/Plugins/Nop.Plugin.Payments.SwedbankPayCheckout/Infrastructure/RouteProvider.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Microsoft.AspNetCore.Routing;
-using Nop.Web.Framework.Mvc.Routing;
-
-namespace Nop.Plugin.Payments.SwedbankPay.Infrastructure
-{
-}
